6d9911a0918c111e6ce0e6cf67a25dd3b1d71658
fran8narf <srlk_doblev@hotmail.com>
2017-08-11 16:11:01 +0200

    nodemon instalado.

--- primer-proyecto/api-restful-favoritos/index.js
+++ primer-proyecto/api-restful-favoritos/index.js
@@ -2,9 +2,14 @@
 
 var express = require('express');
 var bodyParser = require('body-parser'); //MIDDLEWARE - funcionalidad que se ejecuta antes de que se ejecute el script de Node.
-var app = express();
 
-app.listen(3000, function(){
-	console.log("API server Server is working ok on localhost:3000!");
+var app = express();
+var port = process.env.PORT || 3000; //configuración del puerto.
+
+app.use( bodyParser.urlencoded({ extended:false }) ); //función middleware <----
+app.use( bodyParser.json() );
+
+app.listen(port, function(){
+	console.log(`API server Server is working ok on localhost:${port}!`);
 }); //como primer parámetro recibe el puerto que se va a usar.
 
