d4899fd925a648b523801bc0742a2a0b04b965be
fran8narf <srlk_doblev@hotmail.com>
2017-08-11 16:25:13 +0200

    Primera ruta creada y funcionando.

--- primer-proyecto/api-restful-favoritos/index.js
+++ primer-proyecto/api-restful-favoritos/index.js
@@ -9,6 +9,16 @@ var port = process.env.PORT || 3000; //configuración del puerto.
 app.use( bodyParser.urlencoded({ extended:false }) ); //función middleware <----
 app.use( bodyParser.json() );
 
+
+app.get('/', function(req, res){
+	res.status(200).send({
+							message: "Welcome to the Home!", 
+							data: [2,3,4]	
+						});
+});
+//Se pueden añadir parámetros a la url de esta manera: /test
+//Si añadimos una '?' al parámetro, hacemos que ese parámetro sea opcional.
+
 app.listen(port, function(){
 	console.log(`API server Server is working ok on localhost:${port}!`);
 }); //como primer parámetro recibe el puerto que se va a usar.
