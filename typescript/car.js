e75f9aa0a93ed6e4c7a3587282db7cb707697c6b
fran8narf <srlk_doblev@hotmail.com>
2017-08-15 18:55:51 +0200

    POO parte 1

--- typescript/car.js
+++ typescript/car.js
@@ -1,5 +1,6 @@
 var Car = (function () {
     function Car() {
+        this.speed = 0;
     }
     //GETTERS
     Car.prototype.getColour = function () {
@@ -21,14 +22,25 @@ var Car = (function () {
     Car.prototype.setSpeed = function (speed) {
         this.speed = speed;
     };
+    Car.prototype.speedUp = function () {
+        this.speed += 10;
+    };
+    Car.prototype.speedDown = function () {
+        this.speed -= 10;
+    };
     return Car;
 }());
 var carOne = new Car();
+carOne.speedUp();
+carOne.speedUp();
+carOne.speedUp();
+carOne.speedDown();
 var carTwo = new Car();
 var carThree = new Car();
 carOne.setColour("Red");
-carTwo.setColour("Blue");
-carThree.setColour("Orange");
+/*carTwo.setColour("Blue");
+carThree.setColour("Orange");*/
 console.log("El color del coche 1 es: " + carOne.getColour());
-console.log("El color del coche 2 es: " + carTwo.getColour());
-console.log("El color del coche 3 es: " + carThree.getColour());
+console.log("La velocidad del coche 1 es: " + carOne.getSpeed());
+/*console.log("El color del coche 2 es: "+carTwo.getColour());
+console.log("El color del coche 3 es: "+carThree.getColour());*/ 
--- typescript/car.ts
+++ typescript/car.ts
@@ -2,7 +2,7 @@ class Car{
 
 	public colour: string;
 	public model: string;
-	public speed: number;
+	public speed: number = 0;
 
 	//GETTERS
 	public getColour(){
@@ -12,7 +12,7 @@ class Car{
 		return this.model;
 	}
 
-	public getSpeed(){
+	public getSpeed():number{
 		return this.speed;
 	}
 
@@ -27,16 +27,31 @@ class Car{
 		this.speed = speed;
 	}
 
+	public speedUp(){
+		this.speed+= 10;
+	}
+
+	public speedDown(){
+		this.speed-= 10;
+	}
+
 }
 
 var carOne = new Car();
+carOne.speedUp();
+carOne.speedUp();
+carOne.speedUp();
+carOne.speedDown();
+
 var carTwo = new Car();
 var carThree = new Car();
 
 carOne.setColour("Red");
-carTwo.setColour("Blue");
-carThree.setColour("Orange");
+/*carTwo.setColour("Blue");
+carThree.setColour("Orange");*/
 
 console.log("El color del coche 1 es: "+carOne.getColour());
-console.log("El color del coche 2 es: "+carTwo.getColour());
-console.log("El color del coche 3 es: "+carThree.getColour());
+console.log("La velocidad del coche 1 es: "+carOne.getSpeed());
+
+/*console.log("El color del coche 2 es: "+carTwo.getColour());
+console.log("El color del coche 3 es: "+carThree.getColour());*/
