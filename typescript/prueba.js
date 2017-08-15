259e4d6a324c055e010e34d9c4013e45ee2cd268
fran8narf <srlk_doblev@hotmail.com>
2017-08-15 18:22:35 +0200

    diferencias entre var y let | variables y tipos en TS

--- typescript/prueba.js
+++ typescript/prueba.js
@@ -4,3 +4,19 @@ function holaMundo(message) {
 var message = "Bienvenido al mundo de TypeScript cambiando!";
 var tag = document.getElementById("container");
 tag.innerHTML = holaMundo(message);
+//VARIABLES Y TIPOS DE TS
+var nombre = "Fran";
+var edad = 26;
+var programador = true;
+var lenguajes = ["PHP", "JavaScript", "CSS", "HTML5"];
+tag.innerHTML = nombre + " - " + edad + ".";
+//alert("HOLA MUNDO!");
+//DIFERENCIAS ENTRE LET Y VAR para definir variables.
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    var b = 1;
+    console.log("DENTRO DEL IF 'a' vale: " + a_1 + " Y 'b' vale: " + b);
+}
+console.log("FUERA DEL IF 'a' vale: " + a + " Y 'b' vale: " + b);
--- typescript/prueba.ts
+++ typescript/prueba.ts
@@ -8,4 +8,25 @@ var tag = <HTMLElement>document.getElementById("container");
 
 tag.innerHTML = holaMundo(message);
 
-alert("HOLA MUNDO!");
+//VARIABLES Y TIPOS DE TS
+var nombre:string = "Fran";
+var edad:number = 26;
+var programador:boolean = true;
+
+var lenguajes:Array<string> = ["PHP","JavaScript","CSS", "HTML5"];
+
+tag.innerHTML = nombre +" - "+edad+".";
+
+//alert("HOLA MUNDO!");
+
+//DIFERENCIAS ENTRE LET Y VAR para definir variables.
+var a = 7;
+var b = 12;
+
+if ( a === 7 ){
+	let a = 4;
+	var b = 1;
+
+	console.log("DENTRO DEL IF 'a' vale: "+a+" Y 'b' vale: "+b);
+}
+console.log("FUERA DEL IF 'a' vale: "+a+" Y 'b' vale: "+b)
